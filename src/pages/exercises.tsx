4556d08a5c24debfef1a8eeb2a355827f8c2beb0
hamiceis <hamiceis@hotmail.com>
2023-11-13 14:14:39 -0300

    finalizando page exercises

--- src/pages/exercises.tsx
+++ src/pages/exercises.tsx
@@ -3,6 +3,8 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { Button } from "@/components/ui/button";
 import { ArrowLeftCircle } from "lucide-react";
 
+import { exercicios } from "@/lib/exercicioFake";
+
 export function ExercisesPage() {
   const navigate = useNavigate();
   const { id } = useParams();
@@ -21,15 +23,55 @@ export function ExercisesPage() {
       </div>
 
       <div className="mt-5 px-2 py-3 flex flex-col">
-        <div className="grid grid-cols-4 gap-2">
-          {Array.from({ length: 7 }).map((_, i) => {
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
+          {exercicios.map((exercise) => {
             return (
-            <div 
-            className="bg-black h-96 rounded-lg"
-            key={i}>
-              {i + id!}
-            </div>
-              )
+              <div
+                className="px-2 py-3 bg-black h-96 rounded-lg flex flex-col space-y-6 border-2 border-zinc-100 hover:border-2 hover:border-primary transition-colors"
+                key={exercise.id}
+              >
+                <h1 className="font-bold text-center">
+                  {exercise.name_exercise}
+                </h1>
+
+                <div className="flex flex-col gap-4">
+                  <p>
+                    Repetições:
+                    <span className="ml-2 font-semibold text-red-500">
+                      {exercise.repetitions}
+                    </span>
+                  </p>
+                  <p>
+                    Intervalo:
+                    <span className="ml-2 font-semibold text-red-500">
+                      {exercise.interval}
+                    </span>
+                  </p>
+                  <p>
+                    Método:
+                    <span className="ml-2 font-semibold text-red-500">
+                      {exercise.method ?? "Nenhum"}
+                    </span>
+                  </p>
+                  <p>
+                    Carga:
+                    <span className="ml-2 font-semibold text-red-500">{exercise.load}</span>
+                  </p>
+                  <p>
+                    Cadência:
+                    <span className="ml-2 font-semibold text-red-500">
+                      {exercise.cadence}
+                    </span>
+                  </p>
+                  <p className="mt-5">
+                    Observações:
+                    <span className="ml-2 font-extralight text-red-500">
+                      {exercise?.observation}
+                    </span>
+                  </p>
+                </div>
+              </div>
+            );
           })}
         </div>
       </div>
