b5928e5595f2a07d41082821195ba56b9e5eb9f8
hamiceis <hamiceis@hotmail.com>
2023-11-19 13:52:07 -0300

    implementando lógica para de authtenticação da pag

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,14 +1,35 @@
 import { useState, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+
 import { Header } from "@/components/header";
 import { Sidebar } from "@/components/sidebar";
 
-import { Outlet } from "react-router-dom";
+import { AuthTokenProps } from "@/types/authToken.types";
+
+import Cookies from "js-cookie"
 
 export function DashboardPage() {
   const [isMounted, setIsMounted] = useState(false);
+  const [authToken, setAuthToken] = useState<AuthTokenProps | null>(null)
+
+  const navigate = useNavigate()
 
   useEffect(() => {
     setIsMounted(true);
+    const userAuthToken = Cookies.get("authToken");
+
+    //Evita que o usuário acesse outras páginas caso não tenha um authToken, redirecionando para página "/login"
+    if(!userAuthToken){
+      alert("Usuário não autenticado")
+      return navigate("/login")
+    } else {
+      try { 
+        const parsedAuthToken: AuthTokenProps = JSON.parse(userAuthToken)
+        setAuthToken(parsedAuthToken)
+      } catch(error) {
+        console.error("Error parsing authToken:", error);
+      }
+    }
   }, []);
 
   if (!isMounted) {
@@ -22,7 +43,7 @@ export function DashboardPage() {
       </div>
       <div className="bg-zinc-600 h-max md:h-screen w-full md:py-0 p-4">
         <Header />
-        <Outlet />
+        <Outlet context={authToken} />
       </div>
     </div>
   );
