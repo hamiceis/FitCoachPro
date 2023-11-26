d4b1bf3473efc4fa1aa44568db633368ada5a58e
hamiceis <hamiceis@hotmail.com>
2023-11-26 13:27:09 -0300

    removendo linhas

--- src/pages/student.tsx
+++ src/pages/student.tsx
@@ -1,29 +1,27 @@
 import { useParams } from "react-router-dom";
-import { useUsersStore } from "@/hooks/useUsers"
+import { useUsersStore } from "@/hooks/useUsers";
 
 import { StudentSidebar } from "@/components/student-sidebar";
 import { HeaderStudent } from "@/components/header-student";
 import { WorkoutCard } from "@/components/workout-card";
 import { FormWorkout } from "@/components/form-workout";
 
-import { arr } from "@/lib/datafake"
+import { arr } from "@/lib/datafake";
 import { useEffect, useState } from "react";
 import { StudentData } from "@/types/student.types";
 
-
 export function StudentPage() {
-  const [data, setData] = useState<StudentData[] | []>([])
-  const { users } = useUsersStore()
-  const { id } = useParams()
- 
-  useEffect(() => {
-    if(users) {
-      setData(users)
-    }
-  },
-  [users])
+  const [data, setData] = useState<StudentData[] | []>([]);
+  const { users } = useUsersStore();
+  const { id } = useParams();
 
-  const user = data.find((user) => user.id === id)
+  useEffect(() => {
+    if (users) {
+      setData(users);
+    }
+  }, [users]);
+
+  const user = data.find((user) => user.id === id);
 
   return (
     <div className="min-h-screen h-full w-full bg-zinc-600">
@@ -41,13 +39,11 @@ export function StudentPage() {
           <FormWorkout />
         </div>
 
-      
-          <div className="mt-64 md:mt-28 grid grid-cols-3 md:grid-cols-6">
-            {arr.map((workout, i) => (
-              <WorkoutCard data={workout} index={i} key={workout.id + i} />
-            ))}
-          </div>
-      
+        <div className="mt-64 md:mt-28 grid grid-cols-3 md:grid-cols-6">
+          {arr.map((workout, i) => (
+            <WorkoutCard data={workout} index={i} key={workout.id + i} />
+          ))}
+        </div>
       </main>
     </div>
   );
